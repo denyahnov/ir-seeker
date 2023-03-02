ca8369c5ecca095fc9f55b4dc55aaa6f7c87df06
denyahnov <60083582+denyahnov@users.noreply.github.com>
2023-03-03 09:36:42 +1100

    Create Demo_Code.py

---
+++ Demo_Code.py
@@ -0,0 +1,19 @@
+from sensor import IRSeeker360
+
+from ev3dev2.sensor import INPUT_1
+
+# Change '1' to whatever port you are using
+
+sensor = IRSeeker360(1)
+# or you can do this
+sensor = IRSeeker360(INPUT_1)
+
+while True:
+    # This will return a ball angle (0 to 12) and strength
+    angle, strength = sensor.read()
+  
+    print("Angle:", angle)
+    print("Strength:", strength) # The higher the value, the closer it is
+  
+# You should practice closing the sensor after use
+sensor.close()
